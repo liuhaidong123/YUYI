abc1dd65e11c64d0d9f2e45618bc464b8e9422ef
wanyu <728372836@qq.com>
2017-02-23 12:38:19 +0800

    12

--- app/src/main/java/com/technology/yuyi/activity/MS_home_Activity.java
+++ app/src/main/java/com/technology/yuyi/activity/MS_home_Activity.java
@@ -3,6 +3,7 @@ package com.technology.yuyi.activity;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.technology.yuyi.R;
 
@@ -12,5 +13,18 @@ public class MS_home_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ms_home);
+    }
+
+
+
+//    view的点击事件
+    public void ms_homeClick(View v){
+        if (v!=null){
+            switch (v.getId()){
+                case R.id.ms_home_return://首页返回按钮
+                    break;
+            }
+        }
+
     }
 }
