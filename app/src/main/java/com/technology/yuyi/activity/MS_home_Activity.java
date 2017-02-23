57d7e3f362eecea512b17988cadafe998388f66d
wanyu <728372836@qq.com>
2017-02-23 12:14:32 +0800

    12

---
+++ app/src/main/java/com/technology/yuyi/activity/MS_home_Activity.java
@@ -0,0 +1,16 @@
+package com.technology.yuyi.activity;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.technology.yuyi.R;
+
+public class MS_home_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ms_home);
+    }
+}
