1d2a3a474129a20f79c9d748bb47208d6f085605
liuhaidong <1197248097@qq.com>
2017-02-27 20:46:31 +0800

    2月27日 周一晚(修改manifest)

--- app/src/main/java/com/technology/yuyi/lzh_utils/MyGridView.java
+++ app/src/main/java/com/technology/yuyi/lzh_utils/MyGridView.java
@@ -1,8 +1,31 @@
 package com.technology.yuyi.lzh_utils;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.GridView;
+
 /**
  * Created by liuhaidong on 2017/2/27.
  */
+public class MyGridView extends GridView {
+    public MyGridView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
 
-public class MyGridView {
+    public MyGridView(Context context) {
+        super(context);
+    }
+
+    public MyGridView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2,
+                MeasureSpec.AT_MOST);
+        super.onMeasure(widthMeasureSpec, expandSpec);
+    }
 }
+
