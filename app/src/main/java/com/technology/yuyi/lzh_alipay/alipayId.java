7f4227ede9175a77c74e128d336395019be41683
wanyu <728372836@qq.com>
2017-03-07 09:20:03 +0800

    lzh3_7

---
+++ app/src/main/java/com/technology/yuyi/lzh_alipay/alipayId.java
@@ -0,0 +1,38 @@
+package com.technology.yuyi.lzh_alipay;
+
+/**
+ * Created by wanyu on 2017/3/6.
+ */
+
+public interface alipayId {
+    static String testUserName="uercwh7264@sandbox.com";
+    static String testUserPsd="111111";
+    static String PID="";
+    static String APPID="2016080400167695";
+    static String RSA2_PRIVATE="MIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQDA+j84FK7ScKAs" +
+            "fcD1cVxLgPjBLLJGiaizdXNYURP0EFJcx3mg48DOdfoc5OqUpOq2fn63/D69MAC+" +
+            "eKc7/BNqjCPnwNbUslAaeWDPWFLEwhboggV68ZLGwz0KWlcINkL8cUzJhKr1pvjX" +
+            "wM7SNof7IpUuAfwKXV0tozWjj1aZhiuheV1LABjQdVfm3lWAnQoiY1VokgGKORV1" +
+            "EhKk4oY4q179mwIjOUHm48WF9ZC+iaXsylk7D8YYT7khrc+WI2wGov2yKBkssxB7" +
+            "7E862noD/Sy7GnpHo7ZLBv7RFAtWIqoDuYYNL+R+5mmSyzkIlCofU/unpDRKh6IX" +
+            "q6MJN9UbAgMBAAECggEBAKVjE2FV4Ip8edoHoKL6F5aiHw835kYr3oiHJWbQfeBT" +
+            "SLM5RhVMfEa1t7ihvCXOvVXQTXwTVBXCGTEkqHbxUBalqnQJwlShyaMW2ZB6at00" +
+            "VnvISzgV56wvUZYY3LQuBPTjiWLAL+C+zE/kb5ybnaju95lpm/+qCpx9XxAhXZ/O" +
+            "bYk5HtnasTlMvBMlS+etbxvTQP0HTFyS1chqVqmXjbYnc8VCMtv4NV03s3HFAU76" +
+            "b9LZ8ROJ8xO3DLGuR845y4CjvQDdEKQj+vMdpcci8VqP19lnPBGLKUOaTj1Y1+B6" +
+            "0G/Hq3TNqS2KkBZSIMvaIC9SmH3pRgY77EEimN7Fa5ECgYEA9iENWAJXfCt+PMow" +
+            "gAfHUIBA8U3sQezzThy94w6TG6R8L6ohhZy8lWKMaus/6IBU3HhudES6N7r6Afm9" +
+            "22Ea2ujIHhymY0syqmdULQLIpmvMjrRtiUPfyWftg7TXiztAeJERWOePD1IbNwyZ" +
+            "8o3dYyevNPPw43oszJsJiv+P7rkCgYEAyLeAG0/cstBR0HNeDaNqzJxpoFWpOEPy" +
+            "e4wM9dEfoW3e0knMZJdkR2GVNuQ8DJj2hx0o1VFLedZucJoXz1v7ZDJ5OS3hIGDa" +
+            "khFUAOzNhBlV1togev/ptMR9tocgV1DCM7uzSYtx+m4H/vwVoancT5KiRleDxMR6" +
+            "MuBsUg2AWHMCgYEAz82wGnI/stJ1zpVWxYHLwifGrPUBV9faxER3/dA8W8ZQI14c" +
+            "11Yx0l5V95+PYZmYDFyRxRCRuKPgmPJmXWZq6hBHhUTjJkpDCKQc6bPD/N5e4s1q" +
+            "/c2Uj9NzMRPqN99a23B/mPRIUMMdvyigga1NEX5b6vVamHR6hnUPZY/H0eECgYB6" +
+            "I2/JLufVx/6BIjLxiafLqsJ71qSKoI5FsAuS6hadXQZiYJYIADf9V9w88tW3tL0P" +
+            "qybV6ApNsbayvpIOlKJ9blll+g0sbsOSbwj3JyJIaEJb45AwSNomLgtxrvh3KaWD" +
+            "ZYzMDBbnlV04RJ3FKl/t7ATjWQ3OBuF1ZCuGIsEfEQKBgQCTjQs0UvVH0rvU3kBl" +
+            "ZumPwduBhnZiMfjAH/T5CsAOLOaHiG8MmaShJ6fB6KwVmLUAnsz4wTQe9/S6jSBZ" +
+            "t2MwXkdisPg0PbhZuBwZ8Aqd3ki14qDRJLFagA7YjYqLtbF/7MZO7Bk+qRgtoB8G" +
+            "Hy1jxTr7LRTVSKFnFWCS1gne2A==";
+}
